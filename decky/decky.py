4116bcbcb36e564892d9066b4f8b94900dc8edee
Brad Johnson <brad@eaiti.com>
2017-03-13 17:55:50 -0400

    CSS optimizations and documentation.

--- decky/decky.py
+++ decky/decky.py
@@ -232,7 +232,7 @@ def show_entries():
 @app.route('/search')
 def search():
 	db = get_db()
-	cur = db.execute('select * from cards order by name asc limit 50')
+	cur = db.execute('select * from cards order by name asc limit 25')
 	cur_sets = db.execute('select * from sets order by releaseDate desc limit 5')
 	cards = cur.fetchall()
 	sets = cur_sets.fetchall()
