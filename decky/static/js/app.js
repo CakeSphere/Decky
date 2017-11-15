f71a0e45a8fdaead06e61440f6ae0b59d3df23d8
Brad Johnson <bradforddjohnson@gmail.com>
2017-11-15 10:42:47 -0500

    Error messaging.

--- decky/static/js/app.js
+++ decky/static/js/app.js
@@ -52,7 +52,6 @@ $(function () {
           // Update the total quantity with the number of cards added
           deck.totalQuantity = deck.totalQuantity + Number(cardQuantity);
           // Update the quantity display on the UI
-          console.log(deck)
           $('.builder-quantity').text(deck.totalQuantity);
         } else {
           // If card isn't found, flash an error and select the text in the form
